56964fb13108d93cdadbb359d946115434fde0d0
rudkik <66820365+rudkik@users.noreply.github.com>
2021-06-24 14:23:49 +0300

    Вроде как сделал на пк точно надо проверить на мобилке чтоб номрально работало

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var video = document.querySelector('#video');
 
+
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     var constraints = {
+        audio: true,
         video: true
     };
 
@@ -21,148 +23,148 @@ else {
 }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    // References to all the element we will need.
-    var video = document.querySelector('#camera-stream'),
-        image = document.querySelector('#snap'),
-        controls = document.querySelector('.controls'),
-        start_camera = document.querySelector('#start-camera'),
-        take_photo_btn = document.querySelector('#take-photo'),
-        delete_photo_btn = document.querySelector('#delete-photo'),
-        download_photo_btn = document.querySelector('#download-photo'),
-        error_message = document.querySelector('#error-message');
-
-
-
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            var constraints = {
-                video: true
-            };
-
-            navigator.mediaDevices.getUserMedia(constraints)
-                .then(function(stream) {
-                    video.srcObject = stream;
-                    video.onloadedmetadata = function(e) {
-                        video.play();
-                    };
-                    video.onplay = function() {
-                        showVideo();
-                    };
-                })
-                .catch(function(err) {
-                    console.log (err);
-                });
-        }
-        else {
-            console.log ("navigator.mediaDevices not supported")
-        }
-
-    start_camera.addEventListener("click", function(e){
-
-        e.preventDefault();
-
-        // Start video playback manually.
-        video.play();
-        showVideo();
-
-    });
-
-    take_photo_btn.addEventListener("click", function(e){
-
-        e.preventDefault();
-
-        var snap = takeSnapshot();
-
-        // Show image. 
-        image.setAttribute('src', snap);
-        image.classList.add("visible");
-
-        // Enable delete and save buttons
-        delete_photo_btn.classList.remove("disabled");
-        download_photo_btn.classList.remove("disabled");
-
-        // Set the href attribute of the download button to the snap url.
-        download_photo_btn.href = snap;
-
-        // Pause video playback of stream.
-        video.pause();
-
-    });
-
-
-    delete_photo_btn.addEventListener("click", function(e){
-
-        e.preventDefault();
-
-        // Hide image.
-        image.setAttribute('src', "");
-        image.classList.remove("visible");
-
-        // Disable delete and save buttons
-        delete_photo_btn.classList.add("disabled");
-        download_photo_btn.classList.add("disabled");
-
-        // Resume playback of stream.
-        video.play();
-
-    });
-
-
-  
-    function showVideo(){
-        // Display the video stream and the controls.
-
-        hideUI();
-        video.classList.add("visible");
-        controls.classList.add("visible");
-    }
-
-
-    function takeSnapshot(){
-        // Here we're using a trick that involves a hidden canvas element.  
-
-        var hidden_canvas = document.querySelector('canvas'),
-            context = hidden_canvas.getContext('2d');
-
-        var width = video.videoWidth,
-            height = video.videoHeight;
-
-        if (width && height) {
-
-            // Setup a canvas with the same dimensions as the video.
-            hidden_canvas.width = width;
-            hidden_canvas.height = height;
-
-            // Make a copy of the current frame in the video on the canvas.
-            context.drawImage(video, 0, 0, width, height);
-
-            // Turn the canvas image into a dataURL that can be used as a src for our photo.
-            return hidden_canvas.toDataURL('image/png');
-        }
-    }
-
-
-    function displayErrorMessage(error_msg, error){
-        error = error || "";
-        if(error){
-            console.error(error);
-        }
-
-        error_message.innerText = error_msg;
-
-        hideUI();
-        error_message.classList.add("visible");
-    }
-
-   
-    function hideUI(){
-        // Helper function for clearing the app UI.
-        start_camera.classList.remove("visible");
-        controls.classList.remove("visible");
-        video.classList.remove("visible");
-        snap.classList.remove("visible");
-        error_message.classList.remove("visible");
-    }
-
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//
+//     // References to all the element we will need.
+//     var video = document.querySelector('#camera-stream'),
+//         image = document.querySelector('#snap'),
+//         controls = document.querySelector('.controls'),
+//         start_camera = document.querySelector('#start-camera'),
+//         take_photo_btn = document.querySelector('#take-photo'),
+//         delete_photo_btn = document.querySelector('#delete-photo'),
+//         download_photo_btn = document.querySelector('#download-photo'),
+//         error_message = document.querySelector('#error-message');
+//
+//
+//
+//         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//             var constraints = {
+//                 video: true
+//             };
+//
+//             navigator.mediaDevices.getUserMedia(constraints)
+//                 .then(function(stream) {
+//                     video.srcObject = stream;
+//                     video.onloadedmetadata = function(e) {
+//                         video.play();
+//                     };
+//                     video.onplay = function() {
+//                         showVideo();
+//                     };
+//                 })
+//                 .catch(function(err) {
+//                     console.log (err);
+//                 });
+//         }
+//         else {
+//             console.log ("navigator.mediaDevices not supported")
+//         }
+//
+//     start_camera.addEventListener("click", function(e){
+//
+//         e.preventDefault();
+//
+//         // Start video playback manually.
+//         video.play();
+//         showVideo();
+//
+//     });
+//
+//     take_photo_btn.addEventListener("click", function(e){
+//
+//         e.preventDefault();
+//
+//         var snap = takeSnapshot();
+//
+//         // Show image.
+//         image.setAttribute('src', snap);
+//         image.classList.add("visible");
+//
+//         // Enable delete and save buttons
+//         delete_photo_btn.classList.remove("disabled");
+//         download_photo_btn.classList.remove("disabled");
+//
+//         // Set the href attribute of the download button to the snap url.
+//         download_photo_btn.href = snap;
+//
+//         // Pause video playback of stream.
+//         video.pause();
+//
+//     });
+//
+//
+//     delete_photo_btn.addEventListener("click", function(e){
+//
+//         e.preventDefault();
+//
+//         // Hide image.
+//         image.setAttribute('src', "");
+//         image.classList.remove("visible");
+//
+//         // Disable delete and save buttons
+//         delete_photo_btn.classList.add("disabled");
+//         download_photo_btn.classList.add("disabled");
+//
+//         // Resume playback of stream.
+//         video.play();
+//
+//     });
+//
+//
+//
+//     function showVideo(){
+//         // Display the video stream and the controls.
+//
+//         hideUI();
+//         video.classList.add("visible");
+//         controls.classList.add("visible");
+//     }
+//
+//
+//     function takeSnapshot(){
+//         // Here we're using a trick that involves a hidden canvas element.
+//
+//         var hidden_canvas = document.querySelector('canvas'),
+//             context = hidden_canvas.getContext('2d');
+//
+//         var width = video.videoWidth,
+//             height = video.videoHeight;
+//
+//         if (width && height) {
+//
+//             // Setup a canvas with the same dimensions as the video.
+//             hidden_canvas.width = width;
+//             hidden_canvas.height = height;
+//
+//             // Make a copy of the current frame in the video on the canvas.
+//             context.drawImage(video, 0, 0, width, height);
+//
+//             // Turn the canvas image into a dataURL that can be used as a src for our photo.
+//             return hidden_canvas.toDataURL('image/png');
+//         }
+//     }
+//
+//
+//     function displayErrorMessage(error_msg, error){
+//         error = error || "";
+//         if(error){
+//             console.error(error);
+//         }
+//
+//         error_message.innerText = error_msg;
+//
+//         hideUI();
+//         error_message.classList.add("visible");
+//     }
+//
+//
+//     function hideUI(){
+//         // Helper function for clearing the app UI.
+//         start_camera.classList.remove("visible");
+//         controls.classList.remove("visible");
+//         video.classList.remove("visible");
+//         snap.classList.remove("visible");
+//         error_message.classList.remove("visible");
+//     }
+//
+// });
