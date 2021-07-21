6540d6367cde3e40e792331c060ba3a2119a666f
rudkik <66820365+rudkik@users.noreply.github.com>
2021-07-21 09:51:20 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var  take_photo_btn = document.querySelector('#take-photo'),
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     var constraints = {
-        video: { width: 1280, height: 720 }
+        video: { facingMode: { exact: "environment" } }
     };
 
     navigator.mediaDevices.getUserMedia(constraints)
